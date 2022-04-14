70496a7108028a036a0189719fa9d7205054db99
ArieGentry <103394643+ArieGentry@users.noreply.github.com>
2022-04-13 22:30:37 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+public class Main {
+	//obviously everything here is just a showcase that Trie.java should work
+	//Everything here can be deleted to make way for actual input of words into our Trie via file IO, and user IO prompting for searching the Trie
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.addWord("a");
+        trie.addWord("apple");
+        trie.addWord("banner");
+        trie.addWord("bogus");
+        trie.addWord("master");
+        trie.addWord("mine");
+        trie.addWord("man");
+        trie.addWord("criticism");
+        trie.addWord("critic");
+        trie.addWord("collar");
+        trie.addWord("cops");
+        trie.addWord("silver");
+        trie.addWord("boss");
+        trie.addWord("bossy");
+        trie.addWord("flim");
+        trie.addWord("flam");
+        trie.addWord("flak");
+        trie.addWord("pizza");
+        System.out.println(trie.search("bossi"));
+        //checking to see if "a" triggers WORD FOUND for all a-words
+        //checking to see if double matching characters ending words triggers WORD FOUND for words with one less character  that matches the double.
+        //Like, apple ends in e, applee ends in e, but they don't match. There was a while where if would say applee was found, and I fixed it :)
+        System.out.println(trie.search("applee"));
+        System.out.println(trie.search("apples"));
+        System.out.println(trie.search("cope"));
+        System.out.println(trie.search("man"));
+        //OMFG I'm so glad I checked this. When adding critic and criticism, the Trie went c-r-i-t-i- then had a c(true) and c(false) children. 
+        //had to fix that, so that only one c child is created, and if it is later added with isWord=true, it would just change the isWord of that child rather than adding another.
+        System.out.println(trie.search("critic"));
+        System.out.println(trie.search("critics"));
+    }
+}
---
+++ Trie.java
@@ -0,0 +1,140 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trie 
+{
+    private TrieNode rootNode;
+
+    // function that adds words and creates the tree
+    // this should set the root node if necessary
+    public void addWord(String word) 
+    {
+    	//Initializes rootNode. null rootNodes won't work properly. (NullPointer) 
+    	//Maybe we can initialize when we create the Trie instead?
+    	if(rootNode == null)
+    		rootNode = new TrieNode(false, "*");
+    	
+    	//Descend the Trie starting from rootNode
+    	TrieNode current = rootNode;
+    	for(int i = 0; i<word.length(); i++)
+    	{
+    		String character = "" + word.charAt(i);
+    		//This will be the Node we are trying to add
+    		TrieNode next = new TrieNode(false, character);
+    		
+    		//This is the current node's list of children. 
+    		//Used to check if this letter already exists.
+    		ArrayList<TrieNode> children = current.getChildren(current);
+    		boolean childFound = false;
+    		
+    		//If this is the last character of the word, the node we add needs to be isWord=true
+    		if(i == word.length()-1)
+    		{
+    			current.addChild(new TrieNode(true, character));
+    		}
+    		
+    		//loop checking for existing character value amongst children.
+    		for(int j = 0; j < children.size(); j++)
+    		{
+    			if(children.get(j).value.equals(character))
+    			{
+    				current = children.get(j);
+    				childFound = true;
+    				break;
+    			}
+    		}
+    		
+    		//If the current character does not exist as a child, add it and continue through word.
+    		if(childFound == false)
+    		{
+    			current.addChild(next);	
+    			current = next;
+    		}
+    	}
+    }
+
+    // function that searches through the tree and returns possible matches
+    public String search(String word) 
+    {
+    	int depth = -1;
+    	
+    	//Descend the Trie starting from rootNode
+    	TrieNode current = rootNode;
+    	
+    	//boolean that stops searching if no matching child value exists
+    	boolean breakPath = false;
+    	
+    	//progress through word one character at a time
+    	for(int i = 0; i<word.length(); i++) 
+    	{
+    		String character = "" + word.charAt(i);
+    		//Gather all children of current node to check for matching values
+    		ArrayList<TrieNode> children = current.getChildren(current);
+    		
+    		//search children for equivalent node with equivalent String
+    		for(int j = 0; j < children.size(); j++)
+    		{
+    			//if we find matching child, that child is our next node
+    			if(children.get(j).value.equals(character))
+    			{
+    				current = children.get(j);
+    				depth++;
+    				break;
+    			}
+    			//breakPath becomes true if we have exhausted all available children
+    			if(j == children.size()-1)
+    				breakPath = true;
+    		}
+    		//no way to progress further -> stop searching!
+    		if(breakPath)
+    			break;
+    		//if final character happens to be a word...
+    		if(current.isWord && i == word.length()-1 && i == depth)
+    		{
+    			return "Word found: " + word;
+    		}
+    		
+    	}
+    	return "Invalid word/not currently present.";
+    }
+
+    // this class represents a character or string in the Trie tree
+    // it should contain pointers to it's children and a boolean on
+    // if it can make a word complete=
+    public static class TrieNode {
+        public boolean isWord;
+        public String value;
+        public ArrayList<TrieNode> children = new ArrayList(1);
+
+        // can pass a new arraylist to store the children for each node
+        public TrieNode(boolean isWord, String value) {
+            this.isWord = isWord;
+            this.value = value;
+        }
+
+        // adds a TrieNode child to the current node
+        // useful function for adding children
+        public void addChild(TrieNode child) {
+        	for(int j = 0; j < children.size(); j++)
+    		{
+    			if(children.get(j).value.equals(child.value) && child.isWord)
+    			{
+    				children.get(j).isWord = true;
+    				return;
+    			}
+    		}
+        	children.add(child);
+        }
+        
+        //allows Trie.addWord and Trie.search to access children for transversal
+        public ArrayList<TrieNode> getChildren(TrieNode t)
+        {
+        	return t.children;
+        }
+
+        // removes child from children if exists
+        public void removeChild(TrieNode child) {
+            this.children.remove(child);
+        }
+    }
+}
